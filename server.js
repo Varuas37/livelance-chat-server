ea3e1ac8b32158997a01fbe1cbb67fcac05c1f7b
blake robinson <brobs0111@gmail.com>
2022-03-27 14:04:09 -0500

    chat

--- server.js
+++ server.js
@@ -44,11 +44,11 @@ global.onlineUsers = new Map();
 io.on("connection", (socket) => {
     global.chatSocket = socket;
     socket.on("add-user", (userId) => {
-        onlineUsers.set(userId, socket.id);
+        global.onlineUsers.set(userId, socket.id);
     });
 
     socket.on("send-msg", (data) => {
-        const sendUserSocket = onlineUsers.get(data.to);
+        const sendUserSocket = global.onlineUsers.get(data.to);
         if (sendUserSocket) {
             socket.to(sendUserSocket).emit("msg-recieve", data.msg);
         }
