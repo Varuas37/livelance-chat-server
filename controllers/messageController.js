8b30c465cdc15aa14ca5048241e1af8a625a2735
TheVibeKage <marcosmartinez760@gmail.com>
2022-04-09 00:08:09 -0500

    New working chat server

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,29 +1,12 @@
 const Messages = require("../models/messageModel");
-const Users = require("../models/userModel");
 
 module.exports.getMessages = async (req, res, next) => {
   try {
-    // TODO prolly wanna refactor this to be more secure
-    // right now this endpoint gets the messages between any two user ids 
-    // i.e. without security logic, u can fetch anyone two peoples message history
-    // refactor so from value comes from http header/session/jwt
+    const { from, to } = req.body;
 
-    const { from, to, email } = req.body;
-    let foundUserId = to;
-    if(email && !to){
-      console.log("user's email", email);
-      const foundUser = await Users.findOne({
-        email,
-      })
-      foundUserId = foundUser._id
-      console.log("Found userId", foundUserId);
-     
-    }
-    
     const messages = await Messages.find({
       users: {
-        $all: [from, foundUserId],
-      
+        $all: [from, to],
       },
     }).sort({ updatedAt: 1 });
 
@@ -47,23 +30,10 @@ module.exports.addMessage = async (req, res, next) => {
       users: [from, to],
       sender: from,
     });
+
     if (data) return res.json({ msg: "Message added successfully." });
     else return res.json({ msg: "Failed to add message to the database" });
   } catch (ex) {
-    next(ex);
-  }
-};
-//gets users that Current User have messaged
-module.exports.getContacts = async (req, res, next) => {
-  try {
-    
-    const users = await Messages.distinct("users.1", {
-      "users.0": '6238fa1a662e4540705bb4b1'},);
-      console.log("in contacts", users);
-    //TODO need to find user.0 instead of hardcoded
-    return res.json(users);
-  } catch (ex) {
-    console.log("in contacts", ex);
     next(ex);
   }
 };
--- routes/messages.js
+++ routes/messages.js
@@ -1,9 +1,7 @@
-
-const { addMessage, getMessages, getContacts } = require("../controllers/messageController");
+const { addMessage, getMessages } = require("../controllers/messageController");
 const router = require("express").Router();
 
 router.post("/addmsg/", addMessage);
 router.post("/getmsg/", getMessages);
-router.get("/getContacts", getContacts);
 
 module.exports = router;
